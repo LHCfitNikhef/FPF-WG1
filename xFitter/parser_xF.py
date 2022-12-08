2e2e38a6d0a1386c4dcf01f9265f807d402e29f3
Toni Mäkelä <toni.makela@cern.ch>
2022-12-08 16:07:25 +0100

    Updating xFitter table parser

--- xFitter/parser_xF.py
+++ xFitter/parser_xF.py
@@ -8,17 +8,13 @@ exptags = ["FASER$\\nu$ $\\nu_\\mu$", "FASER$\\nu$ $\\bar{\\nu}_\\mu$",
 expstrs = ["FASER#nu, #nu_{#mu}","FASER#nu, #bar{#nu}_{#mu}",
            "FASER#nu2, #nu_{#mu}","FASER#nu2, #bar{#nu}_{#mu}"]
 
-#FIXME Const. toy estimates of unc. scale, to get chi2/N ~ 1
-#TODO  make these bin-by-bin & experimentally motivated!
-expUncs = [0.07, 0.15, 0.015, 0.025]
-
 subdirs=["datafiles/","lhc/","fpf/","neutrinoDIS/","pseudodata/"]
 datadir=""
 for subdir in subdirs:
     datadir+=subdir
     if not os.path.exists(datadir):
         os.mkdir(datadir)
-#FIXME for now, theory grids replaced by constant table directly from Max's results
+#FIXME for now, theory grids replaced by constant table (Max's results)
 if not os.path.exists(datadir+"th/"):
     os.mkdir(datadir+"th/")
 
@@ -29,23 +25,40 @@ for iexp,expID in enumerate(expIDs):
     lines=f.readlines()
     lines.pop(0)    #Remove 2 lines of header
     lines.pop(0)
-    xlo=[]
-    Q2lo=[]
-    Q2hi=[]
+    xlo,xhi,xav=[],[],[]
+    Q2lo,Q2hi,Q2av=[],[],[]
     sigma=[]
-    stat=[]
+    stat,EnuUnc,ElUnc,EhUnc,thetaUnc=[],[],[],[],[]
     for x in lines:
-        xlo.append(float(x.split()[0]))  #Split at whitespace by default
+        xlo.append(float(x.split()[0]))
+        xhi.append(float(x.split()[1]))
+        xav.append(float(x.split()[2]))
         Q2lo.append(float(x.split()[3]))
         Q2hi.append(float(x.split()[4]))
-        sigma.append(10**float(x.split()[9]))  #Convert from log10(sigma)?
-        stat.append(100.*float(x.split()[11])/float(x.split()[10]))
+        Q2av.append(float(x.split()[5]))
+        sigma.append(10.**float(x.split()[9]))  #Convert from log10(sigma)
+        stat.append(float(x.split()[11])/float(x.split()[10]))
+        #Assume perfect detector, uncertainties only in energies and lepton angle
+        #Also assume perfect flavor and sign recognition
+        #Estimates based on J. Rojo's and T. Ariga's slides in FPF5 meeting
+        #EnuUnc.append(0.3)   #Neutrino E unc ~ 30%
+        ElUnc.append(0.3)    #Lepton E unc ~ 30%
+        EhUnc.append(0.4)    #Hadronic FS E unc ~30-50% => avg 40%
+        #theta unc. = 1 mrad, tan(theta) < 0.5, so O(theta unc)>1%. 
+        #Should be bin-dependent, but let's take universal 10% for simplicity
+        #Optimistically relative unc. > 0.001/0.46 and 
+        thetaUnc.append(0.1)
     f.close()
 
     #Vary pseudodata
     sigmavar=[]
     for isig,sig in enumerate(sigma):
-        sigmavar.append(abs(sig*(1. + expUncs[iexp]*np.random.normal())))
+        expUnc  = ElUnc[   isig]**2
+        expUnc += EhUnc[   isig]**2
+        expUnc += thetaUnc[isig]**2
+        #expUnc += EnuUnc[  isig]**2
+        expUnc = np.sqrt(expUnc)
+        sigmavar.append(abs(sig*(1. + expUnc*np.random.normal())))
     
     #Write predictions as a constant value table
     f = open(datadir+"/th/"+expID+"-th.dat", "w")
@@ -58,6 +71,7 @@ for iexp,expID in enumerate(expIDs):
     f.close()
 
     #Write xFitter table
+    expname="FASERv2" if "v2" in exptags[iexp] else "FASERv"
     f = open(datadir+expID+"-thexp.dat", "w")
     f.write("* "+exptags[iexp]+"CC DIS pseudodata\n")
     f.write("&Data\n")
@@ -65,17 +79,18 @@ for iexp,expID in enumerate(expIDs):
     f.write("   IndexDataset = "+str(137+iexp)+"\n") #Assign unique IDs to datasets
     f.write("   Reaction = \'CC nup\'\n\n")
     f.write("   NData = "+str(len(xlo))+"\n")
-    f.write("   NColumn = 6\n")
-    f.write("   ColumnType = 'Flag',3*\'Bin\',\'Sigma\',\'Error\'\n")
-    f.write("   ColumnName = 'binFlag',\'x\',\'Q2min\',\'Q2max\',\'Sigma\',\'stat\'\n\n")
+    f.write("   NColumn = 9\n")
+    f.write("   ColumnType = \'Flag\',3*\'Bin\',\'Sigma\',4*\'Error\'\n")
+    f.write("   ColumnName = \'binFlag\',\'x\',\'Q2min\',\'Q2max\',\'Sigma\',\'stat\',")
+    f.write(               "\'"+expname+"El\',\'"+expname+"theta\',\'"+expname+"Eh\'\n\n")
     f.write("   TheoryType = \'expression\'\n")
     f.write("   TermName   = \'K\'\n")
     f.write("   TermType   = \'reaction\'\n")
     f.write("   TermSource = \'KFactor\'\n")
-    #FIXME below must eventually point to actual prediction grids
+    #FIXME below must eventually point to actual grids
     f.write("   TermInfo   = \'FileName="+datadir+"th/"+expID+"-th.dat:FileColumn=4\'\n")
     f.write("   TheorExpr  = \'K\'\n\n")
-    f.write("   Percent = 1*True\n")
+    f.write("   Percent = 3*True\n")
     f.write("&End\n")
     f.write("&PlotDesc\n")
     f.write("   PlotN = 4\n")
@@ -95,14 +110,20 @@ for iexp,expID in enumerate(expIDs):
     f.write(  " @XTitle: x @YTitle: d^{2}#sigma/dxdQ^{2}")
     f.write(  " @Title:Q^{2}_{min} = 1000 @Ymin:0.01@Xlog@Ylog\'\n")
     f.write("&End\n")
-    f.write("*binFlag        xlo           Q2min           Q2max")
-    f.write("           Sigma            stat\n")
+    f.write("*binFlag       xavg           Q2min           Q2max")
+    f.write("           Sigma            stat")
+    f.write("              El           theta              Eh\n")
     for ix, x in enumerate(xlo):
         f.write("1  ")
-        f.write("{:16.6f}".format(xlo[ix]))
+        f.write("{:16.6f}".format(xav[ix]))
         f.write("{:16.6f}".format(Q2lo[ix]))
         f.write("{:16.6f}".format(Q2hi[ix]))
         f.write("{:16.6e}".format(sigmavar[ix]))
-        f.write("{:16.6f}".format(stat[ix]))
+        #Uncertainties, given in %
+        f.write("{:16.6f}".format(100.*stat[ix]))
+        f.write("{:16.6f}".format(100.*ElUnc[ix]))
+        f.write("{:16.6f}".format(100.*thetaUnc[ix]))
+        f.write("{:16.6f}".format(100.*EhUnc[ix]))
+        
         f.write("\n")
     f.close()
