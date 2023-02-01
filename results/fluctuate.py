e1047b44308f77dab6dcf3479e6dbad6eae172f1
tgiani <s1792848@staffmail.ed.ac.uk>
2023-02-01 12:31:26 +0100

    adding pseudo data from pineappl in NNPDF format

--- results/fluctuate.py
+++ results/fluctuate.py
@@ -2,19 +2,50 @@
 
 import numpy as np
 
-input_files = [
-    "binned_events_FASERv_-14",
+input_files_covmat_nu = [
     "binned_events_FASERv_14",
-    "binned_events_FASERv2_-14",
     "binned_events_FASERv2_14",
 ]
 
-for input_file in input_files:
-    x_avg, Q2_avg, E_nu_avg, log10sigma, stat = np.loadtxt(
-        input_file + ".txt", usecols=(2, 5, 8, 9, 11), unpack=True, skiprows=2
-    )
-    covmat = np.diag(stat)
-    sigma = 10.0**log10sigma
-    sigma_fluctuated = np.random.multivariate_normal(sigma, covmat)
-    fluctuated_res = np.asarray([x_avg, Q2_avg, E_nu_avg, sigma_fluctuated, stat])
-    np.savetxt(input_file + "_fluctuated.txt", fluctuated_res.T)
+input_files_covmat_nub = [
+    "binned_events_FASERv_-14",
+    "binned_events_FASERv2_-14",
+]
+
+input_files_central_values_nu = [
+    "./NNPDF_commondata/pineappl_tables/binned_events_FASERv_14",
+    "./NNPDF_commondata/pineappl_tables/binned_events_FASERv2_14",
+]
+
+
+input_files_central_values_nub = [
+    "./NNPDF_commondata/pineappl_tables/binned_events_FASERv_-14",
+    "./NNPDF_commondata/pineappl_tables/binned_events_FASERv2_-14",
+]
+
+
+def fluctuate_data(files_covmat, files_central_values, flavor):
+
+    # read statistical uncertainty
+    for file_covmat, file_cv in zip(files_covmat, files_central_values):
+        stat = np.loadtxt(file_covmat + ".txt", usecols=(11), unpack=True, skiprows=2)
+        covmat = np.diag(stat)
+
+        x, y, Q2, sigmanu, sigmanub = np.loadtxt(
+            file_cv + ".txt", usecols=(0, 1, 2, 3, 4), unpack=True, skiprows=1
+        )
+        if flavor == 14:
+            sigma_fluctuated = np.random.multivariate_normal(sigmanu, covmat)
+        elif flavor == -14:
+            sigma_fluctuated = np.random.multivariate_normal(sigmanub, covmat)
+        else:
+            print("Only flavors 14 and -14 available")
+
+        fluctuated_res = np.asarray([x, y, Q2, sigma_fluctuated, stat])
+        np.savetxt(file_cv + "_fluctuated.txt", fluctuated_res.T)
+
+
+if __name__ == "__main__":
+
+    fluctuate_data(input_files_covmat_nu, input_files_central_values_nu, 14)
+    fluctuate_data(input_files_covmat_nub, input_files_central_values_nub, -14)
