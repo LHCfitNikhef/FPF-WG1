05004dd1219a167230bde032c32d132a7ec227dd
Toni Makela <toni.makela@cern.ch>
2023-07-03 21:02:10 +0200

    Adding missing files

--- xFitter/PDF_profiling/PDF4LHC21/runFinal.sh
+++ xFitter/PDF_profiling/PDF4LHC21/runFinal.sh
@@ -1,7 +1,13 @@
-for dir in "statOnly" "syst" "systVar05" "uncor"; do
+for dir in "statOnly" "syst" "systVar05" "systVar05El" "uncor"; do
     for subdir in "FASERv2" "FASERv2_inclusive" "FASERv2_charm" "FASERv2_charm_nochargediscrimination" "FASERv2_inclusive_nochargediscrimination" "FASERv2_nochargediscrimination"; do
         cd ${dir}/${subdir}
         sh run.sh
         cd ../..
     done
+done
+
+for dir in "statOnly" "syst" "systVar05" "systVar05El" "uncor"; do
+    cd ${dir}
+    pdflatex collectPlots.tex
+    cd ..
 done
