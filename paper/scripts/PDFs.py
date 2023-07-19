d79d231436a485d35c688a43eba6b54421ea1c26
Juan Rojo <juanrojochacon@gmail.com>
2023-07-19 15:57:19 +0200

    added nnpdf plot

--- paper/scripts/PDFs.py
+++ paper/scripts/PDFs.py
@@ -14,7 +14,7 @@ import scipy
 from scipy.integrate import dblquad
 
 print("\n *********************************************************")
-print("      Charm asymmetry                               ")
+print("      Impact of FPF data                               ")
 print(" ***********************************************************\n")
 
 q = 100 # GeV
@@ -37,7 +37,7 @@ nset=2
 nrep=np.zeros(nset, dtype='int')
 nrep_max = 1000
 
-pdfset=["210713-n3fit-001","NNPDF_FASERV2"]
+pdfset=["230309-ern-002","230718-jcmwfaser-001"]
 
 for iset in range(nset):
 
@@ -71,25 +71,25 @@ for iset in range(nset):
             for ifl in range(nfl):
 
                 if(ifl==0):
-                    # gluon
-                    if(iset==0):
-                        fit1[i-1][ifl][k] = p.xfxQ(0,x,q)
-                    if(iset==1):
-                        fit2[i-1][ifl][k] = p.xfxQ(0,x,q) 
-
-                elif(ifl==1):
                     # uV
                     if(iset==0):
                         fit1[i-1][ifl][k] = p.xfxQ(2,x,q) - p.xfxQ(-2,x,q)
                     if(iset==1):
                         fit2[i-1][ifl][k] = p.xfxQ(2,x,q) - p.xfxQ(-2,x,q)
 
-                elif(ifl==2):
+                elif(ifl==1):
                     # dV
                     if(iset==0):
                         fit1[i-1][ifl][k] = p.xfxQ(1,x,q) - p.xfxQ(-1,x,q)
                     if(iset==1):
                         fit2[i-1][ifl][k] = p.xfxQ(1,x,q) - p.xfxQ(-1,x,q)
+
+                elif(ifl==2):
+                    # gluon
+                    if(iset==0):
+                        fit1[i-1][ifl][k] = p.xfxQ(0,x,q)
+                    if(iset==1):
+                        fit2[i-1][ifl][k] = p.xfxQ(0,x,q) 
 
                 elif(ifl==3):
                     # singlet
@@ -103,18 +103,18 @@ for iset in range(nset):
                             p.xfxQ(3,x,q) + p.xfxQ(-3,x,q)
 
                 elif(ifl==4):
-                    # ubar
+                    # s^+
                     if(iset==0):
-                        fit1[i-1][ifl][k] = p.xfxQ(-2,x,q) 
+                        fit1[i-1][ifl][k] = p.xfxQ(3,x,q) + p.xfxQ(-3,x,q) 
                     if(iset==1):
-                        fit2[i-1][ifl][k] = p.xfxQ(-2,x,q)
+                        fit2[i-1][ifl][k] = p.xfxQ(3,x,q) + p.xfxQ(-3,x,q) 
 
                 elif(ifl==5):
-                    # dbar
+                    # c^+
                     if(iset==0):
-                        fit1[i-1][ifl][k] = p.xfxQ(-1,x,q) 
+                        fit1[i-1][ifl][k] = p.xfxQ(4,x,q) + p.xfxQ(-4,x,q) 
                     if(iset==1):
-                        fit2[i-1][ifl][k] = p.xfxQ(-1,x,q)
+                        fit2[i-1][ifl][k] = p.xfxQ(4,x,q) + p.xfxQ(-4,x,q) 
                                  
                 # end run over sets 
 print("PDF arrays succesfully filled")
@@ -149,12 +149,12 @@ gs = gridspec.GridSpec(nrows,ncols)
 rescolors = py.rcParams['axes.prop_cycle'].by_key()['color']
 
 # pdflabels
-labelpdf=[r"$xg(x,Q)/xg^{(\rm ref)}(x,Q)$",\
-          r"$xu_V(x,Q)/xu_V^{(\rm ref)}(x,Q)$",\
+labelpdf=[r"$xu_V(x,Q)/xu_V^{(\rm ref)}(x,Q)$",\
           r"$xd_V(x,Q)/xd_V^{(\rm ref)}(x,Q)$",\
+          r"$xg(x,Q)/xg^{(\rm ref)}(x,Q)$",\
           r"$x\Sigma(x,Q)/x\Sigma^{(\rm ref)}(x,Q)$",\
-          r"$x\bar{u}(x,Q)/x\bar{u}^{(\rm ref)}(x,Q)$",\
-          r"$x\bar{d}(x,Q)/x\bar{d}^{(\rm ref)}(x,Q)$"]
+          r"$xs^+(x,Q)/xs^+_{(\rm ref)}(x,Q)$",\
+          r"$xc^+(x,Q)/xc^+_{(\rm ref)}(x,Q)$"]
 
 icount=0
 for ifl in range(nfl):
@@ -163,12 +163,14 @@ for ifl in range(nfl):
 
     norm = p1_mid[ifl]
     
-    p1=ax.plot(X,p1_mid[ifl]/norm,ls="dotted")
+    p1=ax.plot(X,p1_mid[ifl]/norm,ls="dotted",color=rescolors[0])
     ax.fill_between(X,p1_high[ifl]/norm,p1_low[ifl]/norm,color=rescolors[0],alpha=0.2)
     p2=ax.fill(np.NaN,np.NaN,color=rescolors[0],alpha=0.2)
 
-    p3=ax.plot(X,p1_mid[ifl]/norm,ls="dashed")
-    ax.fill_between(X,(norm + p2_error[ifl])/norm,(norm - p2_error[ifl])/norm,color=rescolors[1],alpha=0.2)
+    norm = p2_mid[ifl]
+
+    p3=ax.plot(X,p2_mid[ifl]/norm,ls="dashed",color=rescolors[1])
+    ax.fill_between(X,p2_high[ifl]/norm,p2_low[ifl]/norm,color=rescolors[1],alpha=0.2)
     p4=ax.fill(np.NaN,np.NaN,color=rescolors[1],alpha=0.2)
 
 #    p5=ax.plot(X,p3_mid[ifl],ls="solid")
@@ -178,26 +180,25 @@ for ifl in range(nfl):
 
     #ax.set_xscale('linear')
     ax.set_xscale('log')    
-    ax.set_xlim(0.01,0.8)
+    ax.set_xlim(0.004,0.7)
     ax.set_yscale('linear')
     ax.set_ylim(0.8,1.25)
     if(ifl==0):
-        ax.set_ylim(0.96,1.06)
+        ax.set_ylim(0.93,1.07)
     if(ifl==1):
-        ax.set_ylim(0.95,1.08)
+        ax.set_ylim(0.90,1.10)
     if(ifl==2):
         ax.set_ylim(0.95,1.08)
     if(ifl==3):
-        ax.set_ylim(0.95,1.08)
+        ax.set_ylim(0.97,1.03)
     if(ifl==4):
-        ax.set_ylim(0.82,1.15)
+        ax.set_ylim(0.91,1.09)
     if(ifl==5):
         ax.set_ylim(0.82,1.15)
      
     ax.tick_params(which='both',direction='in',labelsize=14,right=True)
     ax.tick_params(which='major',length=8)
     ax.tick_params(which='minor',length=6)
-    ax.set_yticks([0.96,0.98,1.0,1.02,1.04, 1.06,1.08])
     ax.set_ylabel(labelpdf[ifl],fontsize=16)
     if(ifl==1):
         ax.set_xlabel(r'$x$',fontsize=18)
@@ -206,7 +207,7 @@ for ifl in range(nfl):
         ax.text(0.44,0.12,stringQ,fontsize=19,transform=ax.transAxes)
 
     # Add the legend
-    if(ifl==1):
+    if(ifl==2):
         ax.legend([(p1[0],p2[0]),(p3[0],p4[0])],\
                   [r"${\rm Current~Data~(NNPDF4.0)}$",\
                    r"${\rm Current~Data+FASER}\nu 2$"], \
@@ -215,84 +216,8 @@ for ifl in range(nfl):
     icount = icount + 1
 
 py.tight_layout(pad=1.4, w_pad=1.0, h_pad=1.0)
-py.savefig('FASERV2-'+filelabel+'.pdf')
-print('output plot: FASERV2-'+filelabel+'.pdf')
-
-
-
-py.clf()
-ncols,nrows=3,2
-py.figure(figsize=(ncols*5,nrows*3.5))
-gs = gridspec.GridSpec(nrows,ncols)
-rescolors = py.rcParams['axes.prop_cycle'].by_key()['color']
-
-# pdflabels
-labelpdf=[r"$xg(x,Q)/xg^{(\rm ref)}(x,Q)$",\
-          r"$xu_V(x,Q)$",\
-          r"$xd_V(x,Q)/xd_V^{(\rm ref)}(x,Q)$",\
-          r"$x\Sigma(x,Q)/x\Sigma^{(\rm ref)}(x,Q)$",\
-          r"$x\bar{u}(x,Q)/x\bar{u}^{(\rm ref)}(x,Q)$",\
-          r"$x\bar{d}(x,Q)/x\bar{d}^{(\rm ref)}(x,Q)$"]
-
-icount=0
-for ifl in range(nfl):
-
-    ax = py.subplot(gs[icount])
-
-    norm = 1.0
-    
-    p1=ax.plot(X,p1_mid[ifl]/norm,ls="dotted")
-    ax.fill_between(X,p1_high[ifl]/norm,p1_low[ifl]/norm,color=rescolors[0],alpha=0.2)
-    p2=ax.fill(np.NaN,np.NaN,color=rescolors[0],alpha=0.2)
-
-    #p3=ax.plot(X,p2_mid[ifl]/norm,ls="dashed")
-    p3=ax.plot(X,norm/norm,ls="dashed")
-    ax.fill_between(X,(norm + p2_error[ifl])/norm,(norm - p2_error[ifl])/norm,color=rescolors[1],alpha=0.2)
-    p4=ax.fill(np.NaN,np.NaN,color=rescolors[1],alpha=0.2)
-
-#    p5=ax.plot(X,p3_mid[ifl],ls="solid")
-#    ax.fill_between(X,p3_high[ifl],p3_low[ifl],color=rescolors[2],alpha=0.2)
-#    p6=ax.fill(np.NaN,np.NaN,color=rescolors[2],alpha=0.2)
-
-
-    #ax.set_xscale('linear')
-    ax.set_xscale('log')    
-    ax.set_xlim(0.002,xmax)
-    ax.set_yscale('linear')
-    ax.set_ylim(0.8,1.25)
-    if(ifl==0):
-        ax.set_ylim(0.96,1.06)
-    if(ifl==1):
-        ax.set_ylim(0,0.6)
-    if(ifl==3):
-        ax.set_ylim(0.95,1.07)
-    if(ifl==4):
-        ax.set_ylim(0.82,1.15)
-    if(ifl==5):
-        ax.set_ylim(0.82,1.15)
-     
-    ax.tick_params(which='both',direction='in',labelsize=12,right=True)
-    ax.tick_params(which='major',length=7)
-    ax.tick_params(which='minor',length=4)
-    ax.set_ylabel(labelpdf[ifl],fontsize=16)
-    if(ifl==1):
-        ax.set_xlabel(r'$x$',fontsize=18)
-    
-    if(ifl==1):
-        ax.text(0.44,0.87,stringQ,fontsize=18,transform=ax.transAxes)
-
-    # Add the legend
-    if(ifl==1):
-        ax.legend([(p1[0],p2[0]),(p3[0],p4[0])],\
-                  [r"${\rm Current~Data~(NNPDF4.0)}$",\
-                   r"${\rm Current~Data+FASER}\nu 2$"], \
-                  frameon=True,loc=2,prop={'size':14})
-        
-    icount = icount + 1
-
-py.tight_layout(pad=1.4, w_pad=1.0, h_pad=1.0)
-py.savefig('FASERV2abs-'+filelabel+'.pdf')
-print('output plot: FASERV2abs-'+filelabel+'.pdf')
+py.savefig('FASERnu2-'+filelabel+'.pdf')
+print('output plot: FASERnu2-'+filelabel+'.pdf')
 
 exit()
 
