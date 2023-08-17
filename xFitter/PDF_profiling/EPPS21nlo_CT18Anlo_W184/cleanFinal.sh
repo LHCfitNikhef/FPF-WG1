607c20aa139a0657a17b91170ec3de01f3fb8b75
Toni Makela <toni.makela@cern.ch>
2023-08-17 13:36:11 +0200

    Removing old xFitter, to be superseded by updates imported from main

--- xFitter/PDF_profiling/EPPS21nlo_CT18Anlo_W184/cleanFinal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-declare -a dirs=("statOnly" "syst" "systVar05" "systVar05El")
-
-for dir in ${dirs[@]}; do
-    rm ${dir}/collectPlots.pdf
-    cd ${dir}
-    ./clean.sh
-    cd ..
-done
--- xFitter/PDF_profiling/EPPS21nlo_CT18Anlo_W184/syst/FASERv2_nochargediscrimination/run.sh
+++
@@ -1,7 +0,0 @@
-# The commands to perform Hessian PDF profiling w/ xFitter, do e.g.
-#    sh run.sh
-
-ln -s ../../../../datafiles
-
-# Run xFitter:
-xfitter
