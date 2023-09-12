0b730dd5c4b4721b44d7000a9b0738ced5a3907d
Toni Makela <toni.makela@cern.ch>
2023-09-12 14:41:06 +0200

    Retrying xFitter result update

--- xFitter/PDF_profiling/EPPS21nlo_CT18Anlo_W184/cleanFinal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-declare -a dirs=("statOnly" "syst" "fred05" "fcorr05" "fred05fcorr05")
-
-for dir in ${dirs[@]}; do
-    rm ${dir}/collectPlots.pdf
-    cd ${dir}
-    ./clean.sh
-    cd ..
-done
--- xFitter/PDF_profiling/EPPS21nlo_CT18Anlo_W184/statOnly/clean.sh
+++
@@ -1,6 +0,0 @@
-for dir in */; do
-    rm -rf ${dir}/output
-    rm -rf ${dir}/plots
-    rm ${dir}/*.pdf
-    rm ${dir}/unpolarised.wgt
-done
