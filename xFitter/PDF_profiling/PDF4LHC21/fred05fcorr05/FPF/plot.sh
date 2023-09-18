4c7b8a98010b2f8aaf4cf3ada712e4678a502b22
Toni Mäkelä <toni.makela@cern.ch>
2023-09-18 14:59:00 +0200

    Importing all xFitter files from PDF4LHC21 branch

---
+++ xFitter/PDF_profiling/PDF4LHC21/fred05fcorr05/FPF/plot.sh
@@ -0,0 +1,28 @@
+# The commands to visualize results of Hessian PDF profiling w/ xFitter, do e.g.
+#    sh plot.sh
+
+DETECTOR="FPF"
+DETSIMPLE="FPF"
+UNCLVL="fred05fcorr05"
+
+PDF="PDF4LHC21"  #N.B. filenames only! Change also in parameters.yaml
+PDFLONG="PDF4LHC21_40"
+
+PLOTOPTS="--splitplots-pdf --therr --bands --q2all --relative-errors --xrange 1e-3:0.6"
+
+# Visualize results:
+mv plots/plots.pdf plots/plots_OLD.pdf
+xfitter-draw ${PLOTOPTS} output:"Baseline (BL)" profile:../../statOnly/${DETSIMPLE}/output:"BL+${DETECTOR}, stat" profile:output:"BL+${DETECTOR}, stat+syst"
+mv plots/plots.pdf plots/${PDF}_vs_profiled.pdf
+
+cp plots/q2_10000_pdf_uv_ratio.pdf  ${UNCLVL}_${DETSIMPLE}_q2_10000_pdf_uv_ratio.pdf 
+cp plots/q2_10000_pdf_dv_ratio.pdf  ${UNCLVL}_${DETSIMPLE}_q2_10000_pdf_dv_ratio.pdf 
+cp plots/q2_10000_pdf_g_ratio.pdf   ${UNCLVL}_${DETSIMPLE}_q2_10000_pdf_g_ratio.pdf
+cp plots/q2_10000_pdf_Sea_ratio.pdf ${UNCLVL}_${DETSIMPLE}_q2_10000_pdf_Sea_ratio.pdf
+cp plots/q2_10000_pdf_s_ratio.pdf   ${UNCLVL}_${DETSIMPLE}_q2_10000_pdf_s_ratio.pdf
+
+# Produce a new PDF in LHAPDF6 format:
+xfitter-process profile output/pdf_shifts.dat output/pdf_rotate.dat `lhapdf-config --datadir`/${PDFLONG}/ ${PDF}-${DETECTOR}
+
+# Save the new PDF set into LHAPDF6 collection:
+#cp -r ${PDF}-profiled/ `lhapdf-config --datadir`
---
+++ xFitter/gitAdder.sh
@@ -0,0 +1,165 @@
+#!/bin/bash
+declare -a detectors=("FASERv" "FASERv2")
+declare -a datatypes=("charm" "inclusive")
+declare -a parts=("nu" "nub" "nochargediscrimination")
+declare -a unctypes=("statOnly" "syst" "fred05" "fcorr05" "fred05fcorr05")
+declare -a pdfs=("PDF4LHC21" "EPPS21nlo_CT18Anlo_W184")
+
+datapath=xFitter/datafiles/lhc/fpf/neutrinoDIS/pseudodata
+
+#Navigate to git main directory
+cd ..
+
+#Add general scripts
+git add xFitter/parser.cxx
+git add xFitter/gitAdder.sh
+
+for detector in ${detectors[@]}; do
+    for datatype in ${datatypes[@]}; do
+        for part in ${parts[@]}; do
+
+            #Add correlation tables
+            git add ${datapath}/${detector}_${datatype}_${part}.corr
+            git add ${datapath}/${detector}_${datatype}_${part}_fred05.corr
+
+            #Add datatables for preliminary runs
+            git add ${datapath}/prel/${detector}_${datatype}_${part}-thexp.dat
+
+            #Add datatables for final runs
+            for pdf in ${pdfs[@]}; do
+                for unctype in ${unctypes[@]}; do
+                    git add ${datapath}/${pdf}/${unctype}/${detector}_${datatype}_${part}-thexp.dat
+                done
+            done
+
+        done
+    done
+done
+
+#Add PDF_profiling files
+profilingpath=xFitter/PDF_profiling
+declare -a xFrunFiles=("constants.yaml" "parameters.yaml" "steering.txt" "plot.sh" "run.sh")
+declare -a xFprelFiles=("constants.yaml" "parameters.yaml" "steering.txt" "run.sh")
+for pdf in ${pdfs[@]}; do
+
+    if [[ "${pdf}" == "PDF4LHC21" ]]; then
+        declare -a statOnlyDirs=("FASERv" "AdvSND" "FLArE100" "FLArE10" "FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination")
+        declare -a systDirs=("FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination" )
+        declare -a fred05Dirs=("FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination")
+        declare -a fcorr05Dirs=("FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination")
+        declare -a fred05fcorr05Dirs=("AdvSND" "FLArE100" "FLArE10" "FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination")
+    else
+        declare -a statOnlyDirs=("AdvSND" "FLArE100" "FLArE10" "FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination")
+        declare -a systDirs=("FASERv2" "FPF")
+        declare -a fred05Dirs=("FASERv2" "FPF")
+        declare -a fcorr05Dirs=("FASERv2" "FPF")
+        declare -a fred05fcorr05Dirs=("AdvSND" "FLArE100" "FLArE10" "FPF" "FASERv2" "FASERv2_nochargediscrimination" "FASERv2_inclusive" "FASERv2_inclusive_nochargediscrimination" "FASERv2_charm" "FASERv2_charm_nochargediscrimination")
+    fi
+
+    git add ${profilingpath}/${pdf}/cleanFinal.sh
+    git add ${profilingpath}/${pdf}/runFinal.sh
+    git add ${profilingpath}/${pdf}/changeTolerance.cxx
+    
+    #Preliminary runs
+    git add ${profilingpath}/${pdf}/prel/run.sh
+    git add ${profilingpath}/${pdf}/prel/clean.sh
+    for detector in ${detectors[@]}; do
+        for datatype in ${datatypes[@]}; do
+            for part in ${parts[@]}; do    
+                for xFrunFile in ${xFprelFiles[@]}; do
+                    git add ${profilingpath}/${pdf}/prel/${detector}_${datatype}_${part}/${xFrunFile}        
+                done
+                git add ${profilingpath}/${pdf}/prel/${detector}_${datatype}_${part}/output/fittedresults.txt_set_0000
+            done
+        done
+    done
+    
+    #With only statistical uncertainties
+    for subDir in ${statOnlyDirs[@]}; do
+        unctype="statOnly"
+        git add ${profilingpath}/${pdf}/${unctype}/collectPlots.tex                
+        git add ${profilingpath}/${pdf}/${unctype}/run.sh
+        git add ${profilingpath}/${pdf}/${unctype}/clean.sh
+        for xFrunFile in ${xFrunFiles[@]}; do
+            git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${xFrunFile}        
+        done
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_dv_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_g_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_Sea_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_s_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_uv_ratio.pdf        
+        if [[ "${pdf}" == "PDF4LHC21" ]] && [[ "${subDir}" == "FPF" ]]; then
+            git add -A ${profilingpath}/${pdf}/${unctype}/${subDir}/${pdf}-FPF-statOnly
+        fi
+    done
+    
+    #With statistical and systematic uncertainties
+    for subDir in ${systDirs[@]}; do
+        unctype="syst"
+        git add ${profilingpath}/${pdf}/${unctype}/collectPlots.tex                
+        git add ${profilingpath}/${pdf}/${unctype}/run.sh
+        git add ${profilingpath}/${pdf}/${unctype}/clean.sh
+        for xFrunFile in ${xFrunFiles[@]}; do
+            git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${xFrunFile}        
+        done
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_dv_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_g_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_Sea_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_s_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_uv_ratio.pdf        
+    done
+
+    #With statistical and systematic uncertainties (0.5 improvement factor)
+    for subDir in ${fred05Dirs[@]}; do
+        unctype="fred05"
+        git add ${profilingpath}/${pdf}/${unctype}/collectPlots.tex                
+        git add ${profilingpath}/${pdf}/${unctype}/run.sh
+        git add ${profilingpath}/${pdf}/${unctype}/clean.sh
+        for xFrunFile in ${xFrunFiles[@]}; do
+            git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${xFrunFile}        
+        done
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_dv_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_g_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_Sea_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_s_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_uv_ratio.pdf        
+    done
+
+    #With statistical and systematic uncertainties (0.5 correlation factor)
+    for subDir in ${fcorr05Dirs[@]}; do
+        unctype="fcorr05"
+        git add ${profilingpath}/${pdf}/${unctype}/collectPlots.tex                
+        git add ${profilingpath}/${pdf}/${unctype}/run.sh
+        git add ${profilingpath}/${pdf}/${unctype}/clean.sh
+        for xFrunFile in ${xFrunFiles[@]}; do
+            git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${xFrunFile}        
+        done
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_dv_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_g_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_Sea_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_s_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_uv_ratio.pdf        
+    done
+
+    #With statistical and systematic uncertainties (0.5 improvement & correlation factors)
+    for subDir in ${fred05fcorr05Dirs[@]}; do
+        unctype="fred05fcorr05"
+        git add ${profilingpath}/${pdf}/${unctype}/collectPlots.tex                
+        git add ${profilingpath}/${pdf}/${unctype}/run.sh
+        git add ${profilingpath}/${pdf}/${unctype}/clean.sh
+        for xFrunFile in ${xFrunFiles[@]}; do
+            git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${xFrunFile}        
+        done
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_dv_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_g_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_Sea_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_s_ratio.pdf
+        git add ${profilingpath}/${pdf}/${unctype}/${subDir}/${unctype}_`echo ${subDir} | sed 's/_.*//'`_q2_10000_pdf_uv_ratio.pdf        
+        if [[ "${pdf}" == "PDF4LHC21" ]] && [[ "${subDir}" == "FPF" ]]; then
+            git add -A ${profilingpath}/${pdf}/${unctype}/${subDir}/${pdf}-FPF    
+        fi
+    done
+    
+done
+
+echo "gitAdder finished. Remember to commit and push."
