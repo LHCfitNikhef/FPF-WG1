87818b8349caa8489ab8b6715d0a8f42885f65c0
Radonirinaunimi <rrabeman@nikhef.nl>
2024-05-27 15:34:54 +0200

    add charm cards & grids

--- generate_pineappls.sh
+++ generate_pineappls.sh
@@ -7,6 +7,11 @@ NUFPF="/data/theorie/tanjona/miniconda3/envs/nufpf/bin/nufpf"
 ERRLOGS="/data/theorie/tanjona/NNPDF/FPF-WG1/errlogs"
 OUTLOGS="/data/theorie/tanjona/NNPDF/FPF-WG1/outlogs"
 
+PREDICTIONS=(
+  "CHARM charm"
+  # "INCLUSIVE inclusive"
+)
+
 PROJECTILE=(
     nu
     nub
@@ -15,34 +20,41 @@ PROJECTILE=(
 
 MAIN_EXP=(
     FASERv2FCC
-    FASERv2FCC_wide_WithCuts
+    # FASERv2FCC_wide_WithCuts
 )
 
-for prj in "${PROJECTILE[@]}"; do
-    for exp in "${MAIN_EXP[@]}"; do
-        # Remove the main script if it exists, else create it
-        if [[ -e ${exp}_${prj}'.sh' ]]; then
-            rm -rf ${exp}_${prj}.sh
-        else
-            touch ${exp}_${prj}.sh
-        fi
+for type in "${PREDICTIONS[@]}"; do
+  read -a specstype <<< "$type"
+  mtype=${specstype[0]}
+  stype=${specstype[1]}
+  echo "$mtype"
+  echo "$stype"
+  for prj in "${PROJECTILE[@]}"; do
+      for exp in "${MAIN_EXP[@]}"; do
+          # Remove the main script if it exists, else create it
+          if [[ -e ${exp}_${prj}'.sh' ]]; then
+              rm -rf ${exp}_${prj}.sh
+          else
+              touch ${exp}_${prj}.sh
+          fi
 
-        # Define the main commands
-        CMD0="cd /data/theorie/tanjona/NNPDF/FPF-WG1/"
-        CMD1="${NUFPF} xsecs runcards results/INCLUSIVE/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_inclusive_${prj}.txt --no-sgrid --obs XSEC"
-        CMD2="${NUFPF} xsecs grids theory/runcards-${exp}_inclusive_${prj}-a1.tar"
-        CMD3="${NUFPF} xsecs generate_xsecs_datfile theory/grids/grids-${exp}_inclusive_${prj}-a1.tar results/INCLUSIVE/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_inclusive_${prj}.txt 240401-01-rs-nnpdf40like-baseline --no-sgrid"
+          # Define the main commands
+          CMD0="cd /data/theorie/tanjona/NNPDF/FPF-WG1/"
+          CMD1="${NUFPF} xsecs runcards results/${mtype}/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_${stype}_${prj}.txt --no-sgrid --obs XSEC"
+          CMD2="${NUFPF} xsecs grids theory/runcards-${exp}_${stype}_${prj}-a1.tar"
+          CMD3="${NUFPF} xsecs generate_xsecs_datfile theory/grids/grids-${exp}_${stype}_${prj}-a1.tar results/${mtype}/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_${stype}_${prj}.txt 240401-01-rs-nnpdf40like-baseline --no-sgrid"
 
-        # Construct the main script
-        echo $CMD0 >> ${exp}_${prj}.sh
-        echo $CMD1 >> ${exp}_${prj}.sh
-        echo $CMD2 >> ${exp}_${prj}.sh
-        echo $CMD3 >> ${exp}_${prj}.sh
+          # Construct the main script
+          echo $CMD0 >> ${exp}_${prj}.sh
+          echo $CMD1 >> ${exp}_${prj}.sh
+          echo $CMD2 >> ${exp}_${prj}.sh
+          echo $CMD3 >> ${exp}_${prj}.sh
 
-        # # submit the jobs & clean
-        echo "[+] Computing ${exp} with ${prj}."
-        qsub -q short7 -W group_list=theorie -l walltime=04:00:00 -l nodes=1:ppn=4 -l vmem=8gb -e $ERRLOGS -o $OUTLOGS ${exp}_${prj}.sh
-        # sh ${exp}_${prj}.sh
-        rm ${exp}_${prj}.sh
-    done
+          # # submit the jobs & clean
+          echo "[+] Computing ${exp} with ${prj}."
+          qsub -q short7 -W group_list=theorie -l walltime=04:00:00 -l nodes=1:ppn=4 -l vmem=8gb -e $ERRLOGS -o $OUTLOGS ${exp}_${prj}.sh
+          # sh ${exp}_${prj}.sh
+          rm ${exp}_${prj}.sh
+      done
+  done
 done
