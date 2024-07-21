22fe278cf9549ae997b37ac99bf9f4b3ac2ee251
Radonirinaunimi <rrabeman@nikhef.nl>
2024-07-21 15:36:23 +0200

    fix charm predictions

--- generate_pineappls.sh
+++ generate_pineappls.sh
@@ -3,13 +3,14 @@
 # Invoke some binaries
 NUFPF="/data/theorie/tanjona/miniconda3/envs/nufpf/bin/nufpf"
 
-# Paths to write the errors and output logs
-ERRLOGS="/data/theorie/tanjona/NNPDF/FPF-WG1/errlogs"
-OUTLOGS="/data/theorie/tanjona/NNPDF/FPF-WG1/outlogs"
+# Details regarding the job submissions
+NCORES='4'
+WALLTIME='00:20:00'
+MEMORY='8000M'
 
 PREDICTIONS=(
-  # "CHARM charm"
-  "INCLUSIVE inclusive"
+  "CHARM charm"
+  # "INCLUSIVE inclusive"
 )
 
 PROJECTILE=(
@@ -19,9 +20,9 @@ PROJECTILE=(
 )
 
 MAIN_EXP=(
-    # FASERv2FCC
+    FASERv2FCC
     FASERv2FCC_deep
-    # FASERv2FCC_wide
+    FASERv2FCC_wide
     # FASERv2FCC_wide_WithCuts
 )
 
@@ -40,23 +41,51 @@ for type in "${PREDICTIONS[@]}"; do
 
           # Define the main commands
           CMD0="cd /data/theorie/tanjona/NNPDF/FPF-WG1/"
-          CMD1="${NUFPF} xsecs runcards results/${mtype}/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_${stype}_${prj}.txt --no-sgrid --obs XSEC"
+          # TODO: Replace the following `--obs` if not CHARM
+          CMD1="${NUFPF} xsecs runcards results/${mtype}/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_${stype}_${prj}.txt --no-sgrid --obs XSEC_${mtype}"
           CMD2="${NUFPF} xsecs grids theory/runcards-${exp}_${stype}_${prj}-a1.tar"
           CMDX="mv theory/grids-* theory/grids/"
           CMD3="${NUFPF} xsecs generate_xsecs_datfile theory/grids/grids-${exp}_${stype}_${prj}-a1.tar results/${mtype}/${exp}/clipped_nan/clipped_nan_binned_sysevents_${exp}_${stype}_${prj}.txt 240401-01-rs-nnpdf40like-baseline --no-sgrid"
 
           # Construct the main script
+          echo "#!/bin/bash" >> ${exp}_${prj}.sh
           echo $CMD0 >> ${exp}_${prj}.sh
           echo $CMD1 >> ${exp}_${prj}.sh
           echo $CMD2 >> ${exp}_${prj}.sh
           echo $CMDX >> ${exp}_${prj}.sh
           echo $CMD3 >> ${exp}_${prj}.sh
+          chmod +x ${exp}'_'${prj}.sh
 
-          # # submit the jobs & clean
-          echo "[+] Computing ${exp} with ${prj}."
-          qsub -q short7 -W group_list=theorie -l walltime=04:00:00 -l nodes=1:ppn=4 -l vmem=8gb -e $ERRLOGS -o $OUTLOGS ${exp}_${prj}.sh
-          # sh ${exp}_${prj}.sh
-          rm ${exp}_${prj}.sh
+          # Construct CONDOR Commands
+          CONDOR_COMMAND=$PWD'/'${exp}'_'${prj}'.sub'
+          if [ -f "$CONDOR_COMMAND" ] ; then
+            rm $CONDOR_COMMAND
+          else
+            touch $CONDOR_COMMAND
+          fi
+          
+          # Start of CONDOR configurations
+          echo 'executable       = '$PWD'/'${exp}'_'${prj}'.sh'          >> $CONDOR_COMMAND
+          echo 'log              = '$PWD'/logs/'${exp}'_'${prj}'.log'    >> $CONDOR_COMMAND
+          echo 'error            = '$PWD'/errors/'${exp}'_'${prj}'.txt'  >> $CONDOR_COMMAND
+          echo 'output           = '$PWD'/output/'${exp}'_'${prj}'.txt'  >> $CONDOR_COMMAND
+
+          echo 'request_cpus     = '$NCORES                              >> $CONDOR_COMMAND
+          echo 'request_memory   = '$MEMORY                              >> $CONDOR_COMMAND
+          echo '+UseOS           = "el9"'                                >> $CONDOR_COMMAND
+          echo '+JobCategory     = "short"'                              >> $CONDOR_COMMAND
+          echo 'accounting_group = smefit'                               >> $CONDOR_COMMAND
+          echo 'queue'                                                   >> $CONDOR_COMMAND
+
+          # Make script executable
+          chmod +x $CONDOR_COMMAND
+
+          # Submit the Jobs
+          condor_submit $CONDOR_COMMAND
+
+          # Remove remnants of scripts
+          # rm ${exp}_${prj}.sh
+          rm $CONDOR_COMMAND
       done
   done
 done
--- theory/pygrid_parsing.py
+++ theory/pygrid_parsing.py
@@ -7,12 +7,13 @@ import shutil
 from typing import Optional
 
 CURR_PATH = pathlib.Path(__file__).parent
-TARGET_DIR = CURR_PATH.joinpath("parsed_grids")
+TARGET_DIR = CURR_PATH.joinpath("parsed_grids/897")
 TARGET_DIR.mkdir(exist_ok=True)
 
 # Collect all the grids in the directory
 path_grids = CURR_PATH.joinpath("grids")
-all_pgrids = path_grids.glob("*grids-FASERv2FCC_deep_inclusive*.tar")
+# all_pgrids = path_grids.glob("*grids-FASERv2FCC_deep_inclusive*.tar")
+all_pgrids = path_grids.glob("*charm*.tar")
 
 # Map grid to the correct FK table names
 MAP_DATASET_NAMES = {
@@ -20,16 +21,16 @@ MAP_DATASET_NAMES = {
     # "FASERv2FCC_wide_WithCuts_inclusive_nub": 'FASERV2NB_FCC_WIDE_INCLUSIVE',
     # "FASERv2FCC_inclusive_nu": 'FASERV2NU_FCC_INCLUSIVE',
     # "FASERv2FCC_inclusive_nub": 'FASERV2NB_FCC_INCLUSIVE',
-    # "FASERv2FCC_charm_nu": 'FASERV2NU_FCC_charm',
-    # "FASERv2FCC_charm_nub": 'FASERV2NB_FCC_charm',
-    # "FASERv2FCC_deep_charm_nu" : 'FASERV2NU_FCC_DEEP_CHARM',
-    # "FASERv2FCC_deep_charm_nub": 'FASERV2NB_FCC_DEEP_CHARM',
-    # "FASERv2FCC_wide_charm_nu" : 'FASERV2NU_FCC_WIDE_CHARM',
-    # "FASERv2FCC_wide_charm_nub": 'FASERV2NB_FCC_WIDE_CHARM',
+    "FASERv2FCC_charm_nu": 'FASERV2NU_FCC_CHARM',
+    "FASERv2FCC_charm_nub": 'FASERV2NB_FCC_CHARM',
+    "FASERv2FCC_deep_charm_nu" : 'FASERV2NU_FCC_DEEP_CHARM',
+    "FASERv2FCC_deep_charm_nub": 'FASERV2NB_FCC_DEEP_CHARM',
+    "FASERv2FCC_wide_charm_nu" : 'FASERV2NU_FCC_WIDE_CHARM',
+    "FASERv2FCC_wide_charm_nub": 'FASERV2NB_FCC_WIDE_CHARM',
     # "FASERv2FCC_wide_inclusive_nu": 'FASERV2NU_FCC_WIDE_INCLUSIVE',
     # "FASERv2FCC_wide_inclusive_nub": 'FASERV2NB_FCC_WIDE_INCLUSIVE',
-    "FASERv2FCC_deep_inclusive_nu": 'FASERV2NU_FCC_DEEP_INCLUSIVE',
-    "FASERv2FCC_deep_inclusive_nub": 'FASERV2NB_FCC_DEEP_INCLUSIVE',
+    # "FASERv2FCC_deep_inclusive_nu": 'FASERV2NU_FCC_DEEP_INCLUSIVE',
+    # "FASERv2FCC_deep_inclusive_nub": 'FASERV2NB_FCC_DEEP_INCLUSIVE',
 }
 
 
@@ -61,7 +62,8 @@ for grid in all_pgrids:
 
     # Name of the subgrid to copy from
     new_gridname = gridname.removesuffix(f"_{projectile}")
-    correct_name = f"{projectile}_A_1-{new_gridname}-XSFPFCC.pineappl.lz4"
+    # TODO: Remove the `_charm` for inclusive predictions.
+    correct_name = f"{projectile}_A_1-{new_gridname}-XSFPFCC_charm.pineappl.lz4"
     new_correct_name = f"{MAP_DATASET_NAMES[gridname]}.pineappl.lz4"
 
     with tempfile.TemporaryDirectory() as tmpdir:
